eb1f53b4f92a3d2306d6cfb13d53cc4efd1ca714
ChrisChilcoat <cchilc01@gmail.com>
2022-12-26 09:52:23 -0500

    New stie build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Teton 0.0.0
+        Teton 0.0.1
       </header>
     </div>
   );

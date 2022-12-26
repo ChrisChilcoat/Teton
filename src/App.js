0be5d5c57ddfce567ec9b565c7ee82a917bb549d
ChrisChilcoat <cchilc01@gmail.com>
2022-12-26 09:31:58 -0500

    Fresh build

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Teton 0.0.0
+      </header>
+    </div>
+  );
+}
+
+export default App;
